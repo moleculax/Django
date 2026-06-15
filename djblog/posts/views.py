45d85a3e483162bfcf939d1ca2f479c83c00cedd
Emilio J. Gomez <moleculax@gmail.com>
2026-06-15 18:50:47 -0300

    En Desarrollo

--- djblog/posts/views.py
+++ djblog/posts/views.py
@@ -1,20 +1,39 @@
 # from django.shortcuts import render
 # Create your views here.
 # IMPORTAMOS LAS VISTAS
-from importlib.resources import contents
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic.base  import View
+from django.views.generic.base import View
+
 
 class HelloWorld(View):
     def get(self, request):
+        # Array de objetos (lista de diccionarios)
+        data = [
+            {
+                'nombre': 'Pepe Lepon',
+                'edad': 30,
+                'tecnologias': ['Python', 'Django', 'NextJS', 'React'],
+                'activo': True
+            },
+            {
+                'nombre': 'Maria Gomez',
+                'edad': 25,
+                'tecnologias': ['Java', 'Spring Boot', 'Angular'],
+                'activo': True
+            },
+            {
+                'nombre': 'Carlos Lopez',
+                'edad': 35,
+                'tecnologias': ['JavaScript', 'Vue', 'NodeJS'],
+                'activo': False
+            }
+        ]
 
-        data = {
-            'name': 'Pepe lepon',
-            'years': 30,
-            'codes': ['Python','Django','NextJS', 'React']
+        # context debe ser un diccionario con clave 'data'
+        context = {
+            'data': data
         }
 
-
-        # return HttpResponse(contents("Hello World primer django"))
-        return render(request, 'hello_world.html', context=data)
+        # ✅ CORREGIDO: usar context, no data
+        return render(request, 'hello_world.html', context=context)
