0a81845f3f80c2db3a796d4436f3746c8e965b82
Emilio J. Gomez <moleculax@gmail.com>
2026-06-21 23:46:51 -0300

    Modif...

--- movie_api/movie_api/urls.py
+++ movie_api/movie_api/urls.py
@@ -17,8 +17,24 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import  permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+# PARA SWAGGER
+scheme_view = get_schema_view(
+    openapi.Info(
+        title="Movie API",
+        default_version='v1',
+
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+# ===========================================
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("movie/", include("movie.urls")),
+    path("swagger/", scheme_view.with_ui("swagger", cache_timeout=0)),
 ]
