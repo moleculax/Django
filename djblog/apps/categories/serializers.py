cc3dea7d426daca1972a6e63a98e8646dc887626
Emilio J. Gomez <moleculax@gmail.com>
2026-06-27 17:13:52 -0300

    Delete djblog directory

--- djblog/apps/categories/serializers.py
+++
@@ -1,9 +0,0 @@
-
-# ESTE ARCHIVO SE CREA A PIE DESDE CERO
-from rest_framework import serializers
-from apps.categories.models import Category
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
-
--- djblog/apps/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from apps.categories.models import Category
-from apps.categories.serializers import CategorySerializer
-
-class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
